e89ef046a2f60d0853daa1e6d5d61ecabdc08d20
Ashwani Yadav <ashyadavash@gmail.com>
2020-01-15 01:57:03 +0530

    Updates

---
+++ precache-manifest.197c274d9fbc392ce94ea165f9278cd8.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "bd6820f1a4015e9645e470ea8e7b1501",
+    "url": "/do-dev/static/media/anime3.bd6820f1.png"
+  },
+  {
+    "revision": "6661dfd8208ccd53fd24",
+    "url": "/do-dev/static/css/main.95e8472a.chunk.css"
+  },
+  {
+    "revision": "b17a118e13e53558658b681a0ebdad82",
+    "url": "/do-dev/static/media/nucleo.b17a118e.ttf"
+  },
+  {
+    "revision": "ce94e87d31a9971e87b9",
+    "url": "/do-dev/static/js/1.ce94e87d.chunk.js"
+  },
+  {
+    "revision": "b1f33966d902b2b8b38a",
+    "url": "/do-dev/static/js/runtime~main.b1f33966.js"
+  },
+  {
+    "revision": "eb6be414b75c412c13eeee2d2e8390cb",
+    "url": "/do-dev/static/media/react-logo.eb6be414.png"
+  },
+  {
+    "revision": "6661dfd8208ccd53fd24",
+    "url": "/do-dev/static/js/main.6661dfd8.chunk.js"
+  },
+  {
+    "revision": "9fcf69e5359987c9b1545aae1c2c5faa",
+    "url": "/do-dev/static/media/emilyz.9fcf69e5.jpg"
+  },
+  {
+    "revision": "03ef1918e505c3e3471f9369ef7a638f",
+    "url": "/do-dev/static/media/nucleo.03ef1918.eot"
+  },
+  {
+    "revision": "f0b489a5dbbff08833d21024f9fcbd4e",
+    "url": "/do-dev/static/media/nucleo.f0b489a5.woff"
+  },
+  {
+    "revision": "5987dd12fea78ce5f97ae601b08ec03c",
+    "url": "/do-dev/static/media/nucleo.5987dd12.woff2"
+  },
+  {
+    "revision": "ce94e87d31a9971e87b9",
+    "url": "/do-dev/static/css/1.eb10064a.chunk.css"
+  },
+  {
+    "revision": "c0e51c049ca06002ef20912a09ad711d",
+    "url": "/do-dev/index.html"
+  }
+];
