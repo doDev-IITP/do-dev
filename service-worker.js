02fd2f8d4f41dd47d15f9a55a3a0e980d8e7c4f2
Ashwani Yadav <ashyadavash@gmail.com>
2020-01-15 03:40:02 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/do-dev/precache-manifest.f57e1f708deb6cb9afd0233a27469b07.js"
+  "/precache-manifest.b687fc95d2eeff6150a936ee0b454ccb.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/do-dev/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
