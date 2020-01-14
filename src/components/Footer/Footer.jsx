1337867b85c6116a87a7760f7aba94b63dea3ff3
Ashwani Yadav <ashyadavash@gmail.com>
2020-01-15 02:21:11 +0530

    basic refractoring

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,20 +1,3 @@
-/*!
-
-=========================================================
-* Black Dashboard React v1.0.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/black-dashboard-react
-* Copyright 2019 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/black-dashboard-react/blob/master/LICENSE.md)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
 /*eslint-disable*/
 import React from "react";
 // used for making the prop types of this component
@@ -25,12 +8,13 @@ import { Container, Row, Nav, NavItem, NavLink } from "reactstrap";
 
 class Footer extends React.Component {
   render() {
+    let title = "do{Dev}";
     return (
       <footer className="footer">
         <Container fluid>
           <Nav>
             <NavItem>
-              <NavLink href="javascript:void(0)">Creative Tim</NavLink>
+    <NavLink href="javascript:void(0)">{title}</NavLink>
             </NavItem>
             <NavItem>
               <NavLink href="javascript:void(0)">About Us</NavLink>
@@ -47,7 +31,7 @@ class Footer extends React.Component {
               rel="noopener noreferrer"
               target="_blank"
             >
-              Creative Tim
+              {title}
             </a>{" "}
             for a better web.
           </div>
